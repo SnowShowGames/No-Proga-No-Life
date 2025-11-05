4c65b5a1bdd53c11b82940139bf4c0a1a37a2abf
Snow Show Games <kaba4ok42@gmail.com>
2025-11-05 13:49:07 +0300

    CC+
    Пытались сделать первый номер всем селом, но не смогли ;-(

---
+++ newfile_withoutkr03.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cmath>
+#include <clocale>
+
+using namespace std;
+
+//straniza 31 number 1
+int pr(int a[3], int &n)
+{
+	for (int i = 1; i < n; i++)
+	{
+		int k = 0;
+		if (a[i - 1] > a[i]) return k;
+		else return k = 1;
+	}
+	//	cout << a[i] << " ";
+	//cout << "\n";
+}
+float pr2(int a[3], int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += a[i];
+	}
+	float sr = sum / n;
+	return sr;
+}
+int pr3(int a[3], int n)
+{
+	int m;
+	cout << "¬ведите m"; //chetn and nechent
+	cin >> m;
+	if (m == 0)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			if (a[i] % 2 == 0)
+			{
+				a[i] += pr2(a, n);
+			}
+		}
+	}
+	if (m == 1)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			if (a[i] % 2 != 0)
+			{
+				a[i] += pr2(a, n);
+			}
+		}
+	}
+	return a;
+}
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int a[3] = { 1,2,3 }, b[3] = {8,1,6}, n = 3;
+	if (pr(a, n) + pr(b, n) == 2)
+	{
+		a[3] = pr3(a, n);
+		b[3] = pr3(b, n);
+	}
+	cout << a[3] << " " << "\n" << b[3] << " " << endl;
+}
