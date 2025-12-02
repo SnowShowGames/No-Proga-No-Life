7cddc512f080273ab026224a95213dde0547f1d5
Snow Show Games <kaba4ok42@gmail.com>
2025-12-03 01:08:00 +0300

    C++

---
+++ newfile_na07.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s1 = "1n31j*ksnsk*mdikeik*mxsndm**mxneik";
+    string s2;
+
+    for (int i = 0; i < s1.size(); i++)
+    {
+        if (s1[i] == '*')
+        {
+            s2 += "??";
+        }
+        else
+        {
+            s2 += s1[i];
+        }
+    }
+
+    cout << s2 << endl;
+
+}
